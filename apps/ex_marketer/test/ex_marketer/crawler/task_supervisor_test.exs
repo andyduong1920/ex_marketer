31f6f2de1250066fa5ede8d9f08ac444d2e0a6b3
An Duong <duonghienan.dha@gmail.com>
2020-07-16 14:08:21 +0700

    Allow TaskSupervisor receive a list keywords

--- apps/ex_marketer/test/ex_marketer/crawler/task_supervisor_test.exs
+++ apps/ex_marketer/test/ex_marketer/crawler/task_supervisor_test.exs
@@ -9,7 +9,18 @@ defmodule ExMarketer.Crawler.TaskSupervisorTest do
       use_cassette "google/valid" do
         {:ok, pid} = TaskSupervisor.start_chilld("grammarly")
 
-        assert pid !== nil
+        assert is_pid(pid)
+      end
+    end
+  end
+
+  describe "given a list keywords" do
+    test 'start_chilld/1 spawn a list new process' do
+      use_cassette "google/valid_list" do
+        [{:ok, pid_1}, {:ok, pid_2}] = TaskSupervisor.start_chilld(["grammarly", "developer"])
+
+        assert is_pid(pid_1)
+        assert is_pid(pid_2)
       end
     end
   end
@@ -19,7 +30,7 @@ defmodule ExMarketer.Crawler.TaskSupervisorTest do
       use_cassette "google/invalid" do
         {:ok, pid} = TaskSupervisor.start_chilld("invalid")
 
-        assert pid !== nil
+        assert is_pid(pid)
       end
     end
   end
