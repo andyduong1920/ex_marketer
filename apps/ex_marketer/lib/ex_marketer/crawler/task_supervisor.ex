48d5c28027e008f77b3e70dd0fda914c075c2de5
An Duong <duonghienan.dha@gmail.com>
2020-07-16 13:57:42 +0700

    Adding task_supervisor_test

--- apps/ex_marketer/lib/ex_marketer/crawler/task_supervisor.ex
+++ apps/ex_marketer/lib/ex_marketer/crawler/task_supervisor.ex
@@ -2,7 +2,7 @@ defmodule ExMarketer.Crawler.TaskSupervisor do
   alias ExMarketer.Crawler.Worker
 
   def start_chilld(keyword) do
-    # TODO: Store the keyword to db
+    # TODO: Store the keyword into the Database and passing the record id the worker
     record_id = 1
 
     Task.Supervisor.start_child(
