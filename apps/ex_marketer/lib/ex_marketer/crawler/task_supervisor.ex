fd59b983128453bdcb100fe489bdc362e3a0c3d6
An Duong <duonghienan.dha@gmail.com>
2020-07-16 14:08:21 +0700

    Allow TaskSupervisor receive a list keywords

--- apps/ex_marketer/lib/ex_marketer/crawler/task_supervisor.ex
+++ apps/ex_marketer/lib/ex_marketer/crawler/task_supervisor.ex
@@ -1,7 +1,11 @@
 defmodule ExMarketer.Crawler.TaskSupervisor do
   alias ExMarketer.Crawler.Worker
 
-  def start_chilld(keyword) do
+  def start_chilld(keywords) when is_list(keywords) do
+    keywords |> Enum.map(&start_chilld(&1))
+  end
+
+  def start_chilld(keyword) when is_binary(keyword) do
     # TODO: Store the keyword into the Database and passing the record id the worker
     record_id = 1
 
