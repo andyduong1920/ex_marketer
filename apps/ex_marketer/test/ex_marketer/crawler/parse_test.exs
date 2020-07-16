12c77975273fdc3a393396cf2bc215b34f0028df
An Duong <duonghienan.dha@gmail.com>
2020-07-16 10:39:47 +0700

    Chaning the vcr

--- apps/ex_marketer/test/ex_marketer/crawler/parse_test.exs
+++ apps/ex_marketer/test/ex_marketer/crawler/parse_test.exs
@@ -8,7 +8,7 @@ defmodule ExMarketer.Crawler.ParseTest do
 
   test "perform/1 returns the ExMarketer.Crawler.Result" do
     use_cassette "google/valid" do
-      {:ok, response_body} = Request.get("chay quang cao")
+      {:ok, response_body} = Request.get("grammarly")
 
       assert Parse.perform(response_body) === expecting_result()
     end
@@ -17,24 +17,22 @@ defmodule ExMarketer.Crawler.ParseTest do
   defp expecting_result do
     %Result{
       raw_html: raw_html(),
-      total_ads_on_top: 4,
-      total_non_ads: 7,
-      total_ads: 7,
-      total_link: 143,
-      ads_on_top_link: [
-        "https://ads.google.com/intl/vi_vn/getstarted/",
-        "https://doitacgoogle.com/",
-        "https://quangcaogoogleuytin.com/",
-        "https://one.adsplus.vn/"
-      ],
+      total_ads_on_top: 1,
+      total_non_ads: 10,
+      total_ads: 1,
+      total_link: 106,
+      ads_on_top_link: ["https://www.grammarly.com/"],
       non_ads_link: [
-        "https://thanhthinhbui.com/cach-chay-quang-cao-tren-facebook-hieu-qua/",
-        "https://www.sapo.vn/blog/huong-dan-chi-tiet-cach-tao-quang-cao-facebook-hieu-qua/",
-        "https://www.facebook.com/groups/399124403873008/",
-        "https://moavietnam.com/lam-the-nao-de-quang-cao-tren-facebook-hieu-qua",
-        "http://caohuymanh.com/internet-marketing/facebook-marketing/huong-dan-chay-quang-cao-facebook-tu-a-z.html",
-        "https://kiemtiencenter.com/tao-tai-khoan-quang-cao-facebook-ads/",
-        "https://seotot.vn/chay-quang-cao-facebook.html"
+        "https://www.grammarly.com/",
+        "https://www.grammarly.com/grammar-check",
+        "https://www.grammarly.com/keyboard",
+        "https://chrome.google.com/webstore/detail/grammarly-for-chrome/kbfnbcaeplbcioakkpcpgfkobkghlhen",
+        "https://chrome.google.com/webstore/detail/grammarly-for-chrome/kbfnbcaeplbcioakkpcpgfkobkghlhen?hl=vi",
+        "https://play.google.com/store/apps/details?id=com.grammarly.android.keyboard&hl=vi",
+        "https://en.wikipedia.org/wiki/Grammarly",
+        "https://apps.apple.com/us/app/grammarly-keyboard/id1158877342",
+        "https://www.grammarcheck.net/editor/",
+        "https://quantrimang.com/cach-su-dung-grammarly-kiem-tra-tieng-anh-tren-microsoft-word-127626"
       ]
     }
   end
