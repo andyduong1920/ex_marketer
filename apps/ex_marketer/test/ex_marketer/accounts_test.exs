89af56380554ea544b4a8a3920941f0782eb01c0
An Duong <duonghienan.dha@gmail.com>
2020-07-28 09:35:30 +0700

    Minor refactor

--- apps/ex_marketer/test/ex_marketer/accounts_test.exs
+++ apps/ex_marketer/test/ex_marketer/accounts_test.exs
@@ -1,5 +1,5 @@
 defmodule ExMarketer.AccountsTest do
-  use ExMarketer.DataCase
+  use ExMarketer.DataCase, async: true
 
   alias ExMarketer.Accounts
   import ExMarketer.AccountsFactory
@@ -58,11 +58,11 @@ defmodule ExMarketer.AccountsTest do
     end
 
     test "validates email and password when given" do
-      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "not valid"})
+      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "short"})
 
       assert %{
                email: ["must have the @ sign and no spaces"],
-               password: ["should be at least 12 character(s)"]
+               password: ["should be at least 6 character(s)"]
              } = errors_on(changeset)
     end
 
@@ -234,12 +234,12 @@ defmodule ExMarketer.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.update_user_password(user, valid_user_password(), %{
-          password: "not valid",
+          password: "short",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 6 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
@@ -443,12 +443,12 @@ defmodule ExMarketer.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.reset_user_password(user, %{
-          password: "not valid",
+          password: "short",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 6 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
