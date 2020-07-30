ff717096c9459f62b39f488f4fbd06b1b7628897
An Duong <duonghienan.dha@gmail.com>
2020-07-30 18:20:54 +0700

    Adding ui test for login notificate

--- apps/ex_marketer_web/test/ex_marketer_web/features/accounts/login_test.exs
+++ apps/ex_marketer_web/test/ex_marketer_web/features/accounts/login_test.exs
@@ -9,4 +9,31 @@ defmodule ExMarketerWeb.Accounts.LoginTest do
     |> assert_has(Query.css(".app-layout"))
     |> assert_has(Query.text(user.email))
   end
+
+  @sessions 3
+  feature "get notify when the other people login", %{sessions: [session_1, session_2, session_3]} do
+    user_1 = insert(:user)
+    user_2 = insert(:user)
+    user_3 = insert(:user)
+
+    session_1
+    |> login(user_1.email, valid_user_password())
+
+    session_2
+    |> login(user_2.email, valid_user_password())
+
+    session_3
+    |> login(user_3.email, valid_user_password())
+
+    # TODO: Test the notificate message
+
+    session_1
+    |> assert_has(Query.css(".notification .alert"))
+
+    session_2
+    |> assert_has(Query.css(".notification .alert"))
+
+    session_3
+    |> refute_has(Query.css(".notification .alert"))
+  end
 end
