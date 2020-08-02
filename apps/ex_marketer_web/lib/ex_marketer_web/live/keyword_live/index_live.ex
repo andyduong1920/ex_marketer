ef43f3dfec70400cbd6fd7095e684302437f9d1d
An Duong <duonghienan.dha@gmail.com>
2020-08-02 17:33:35 +0700

    file upload

--- apps/ex_marketer_web/lib/ex_marketer_web/live/keyword_live/index_live.ex
+++ apps/ex_marketer_web/lib/ex_marketer_web/live/keyword_live/index_live.ex
@@ -6,11 +6,18 @@ defmodule ExMarketerWeb.KeywordLive.IndexLive do
   def mount(_params, %{"current_user_id" => current_user_id}, socket) do
     if connected?(socket), do: subscribe_pubsub_channel(current_user_id)
 
-    {
-      :ok,
-      assign(socket, :keywords, Keyword.list_by_user(current_user_id)),
-      temporary_assigns: [keywords: []]
-    }
+    socket =
+      socket
+      |> assign(:keywords, Keyword.list_by_user(current_user_id))
+      |> assign(:changeset, Keyword.upload_keyword_changeset())
+
+    {:ok, socket, temporary_assigns: [keywords: []]}
+  end
+
+  def handle_event("save", %{"keyword" => %{"file" => file}}, socket) do
+    # Keyword.upload_keyword_changeset(file)
+
+    {:noreply, socket}
   end
 
   def handle_info({:keyword_completed, %{keyword_id: keyword_id}}, socket) do
