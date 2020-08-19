bd58be41eab2370a473e6d27b9117b5cd9ba1ad7
An Duong <duonghienan.dha@gmail.com>
2020-08-19 14:39:11 +0700

    migrate to handle continue

--- apps/ex_marketer_web/lib/ex_marketer_web/controllers/keyword_controller.ex
+++ apps/ex_marketer_web/lib/ex_marketer_web/controllers/keyword_controller.ex
@@ -23,4 +23,11 @@ defmodule ExMarketerWeb.KeywordController do
       |> halt()
     end
   end
+
+  def create(conn, _) do
+    conn
+    |> redirect(to: Routes.keyword_index_path(conn, :index))
+    |> put_flash(:error, "aaa")
+    |> halt()
+  end
 end
