78d12937b83a7ef06047a2321f93ea13eab8e7bc
An Duong <duonghienan.dha@gmail.com>
2020-08-19 14:57:53 +0700

    minor refactor

--- apps/ex_marketer/lib/ex_marketer/crawler/worker.ex
+++ apps/ex_marketer/lib/ex_marketer/crawler/worker.ex
@@ -21,10 +21,11 @@ defmodule ExMarketer.Crawler.Worker do
   # Just a demonstrate for handle_continue, it run before the perform
   @impl true
   def handle_continue(:get_keyword, state) do
-    keyword_record = find_keyword(state.keyword_id)
+    keyword_record = Keyword.find(state.keyword_id)
 
     new_state =
       state
+      |> Map.put(:keyword_record, keyword_record)
       |> Map.put(:keyword, keyword_record.keyword)
       |> Map.put(:user_id, keyword_record.user_id)
 
@@ -33,11 +34,11 @@ defmodule ExMarketer.Crawler.Worker do
 
   @impl true
   def handle_continue(:perform, state) do
-    on_start(state.keyword_id)
+    on_start(state.keyword_record)
 
     {:ok, response_body} = google_client().get(state.keyword)
 
-    on_success(state.keyword_id, Parse.perform(response_body))
+    on_success(state.keyword_record, Parse.perform(response_body))
 
     {:stop, :normal, state}
   end
@@ -51,49 +52,39 @@ defmodule ExMarketer.Crawler.Worker do
   end
 
   def terminate(reason, state) do
-    on_fail(state.keyword_id, reason)
+    on_fail(state.keyword_record, reason)
   end
 
   defp google_client, do: Application.get_env(:ex_marketer, :google_client)
 
-  defp on_start(keyword_id) do
-    keyword = find_keyword(keyword_id)
-
-    keyword
+  defp on_start(keyword_record) do
+    keyword_record
     |> Keyword.update!(%{status: Keyword.statues().in_progress})
 
-    broadcast_to_user(keyword.user_id, keyword_id)
+    broadcast_to_user(keyword_record)
   end
 
-  defp on_success(keyword_id, result) do
-    keyword = find_keyword(keyword_id)
-
-    keyword
+  defp on_success(keyword_record, result) do
+    keyword_record
     |> Keyword.update!(%{status: Keyword.statues().completed, result: Map.from_struct(result)})
 
-    broadcast_to_user(keyword.user_id, keyword_id)
+    broadcast_to_user(keyword_record)
   end
 
-  defp on_fail(keyword_id, ex) do
+  defp on_fail(keyword_record, ex) do
     {{reason, _}, _} = ex
 
-    keyword = find_keyword(keyword_id)
-
-    keyword
+    keyword_record
     |> Keyword.update!(%{status: Keyword.statues().failed, failure_reason: Atom.to_string(reason)})
 
-    broadcast_to_user(keyword.user_id, keyword_id)
+    broadcast_to_user(keyword_record)
   end
 
-  defp find_keyword(keyword_id) do
-    Keyword.find(keyword_id)
-  end
-
-  defp broadcast_to_user(user_id, keyword_id) do
+  defp broadcast_to_user(keyword_record) do
     Phoenix.PubSub.broadcast!(
       ExMarketer.PubSub,
-      "user_keyword:#{user_id}",
-      {:keyword_completed, %{keyword_id: keyword_id}}
+      "user_keyword:#{keyword_record.user_id}",
+      {:keyword_completed, %{keyword_id: keyword_record.id}}
     )
   end
 
