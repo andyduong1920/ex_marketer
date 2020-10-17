bbe8b2af85fb2948b5543b76f8e0f697c73858ed
An Duong <andyduong1920@gmail.com>
2020-10-17 20:59:12 +0700

    upgrade phoenix_live_view

--- apps/ex_marketer_web/mix.exs
+++ apps/ex_marketer_web/mix.exs
@@ -42,7 +42,7 @@ defmodule ExMarketerWeb.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_view, "~> 0.14.4"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.3.1"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
